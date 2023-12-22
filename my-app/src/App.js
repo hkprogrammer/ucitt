a2d0fa77783566ee2416f84ee599c4aba15c7332
Nathan Pham <phamnathan40@gmail.com>
2023-12-21 22:35:06 -0800

    Changed the words and links a bit on default react homepage just to experiment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to UCITT Software in Development.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Watch Youtube
         </a>
       </header>
     </div>
