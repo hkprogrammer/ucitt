f6773d057922d72bfad84545a2ebd9bacd5ef36c
phamster <phamnathan40@gmail.com>
2023-12-23 21:55:10 -0800

    Homepage test

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to UCITT Software in Development.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Watch Youtube
-        </a>
-      </header>
-    </div>
+    <Home />
   );
 }
 
---
+++ front/src/Home.js
@@ -0,0 +1,13 @@
+// Homepage Stuff
+
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            Test Homepage Change
+        </div>
+    );
+}
+
+export default Home;
