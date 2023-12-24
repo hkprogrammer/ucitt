e48a786231ada7fbe8c3d143963030e04ccb8dbb
phamster <phamnathan40@gmail.com>
2023-12-23 23:27:42 -0800

    Basic NavBar (Not much CSS)

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
-    <Home />
+    <NavBar />
   );
 }
 
