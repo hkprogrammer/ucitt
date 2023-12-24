93ddbedd2ee6f93d3debd5f5a5eb4bd54708ae5c
phamster <phamnathan40@gmail.com>
2023-12-23 23:39:47 -0800

    Added basic navbar to home page

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
-import NavBar from './Components/NavBar';
 
 function App() {
   return (
-    <NavBar />
+    <Home />
   );
 }
 
