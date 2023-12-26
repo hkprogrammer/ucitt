e9cb6e85cced2b00d73542dbf7b1a9d392ada20a
robinjiang6 <robinjiang6@gmail.com>
2023-12-26 17:33:42 -0500

    events page first draft

--- front/src/Components/Event.js
+++ front/src/Components/Event.js
@@ -11,28 +11,48 @@ function Event(props) {
     *   props.capacity      EX: "5/16"
     *   props.location      EX: "Santa Monica College"
     * 
-    *   props.registrationCloseDate EX: "12/25/2023 10:00 PST";
-    *       ^ MUST BE IN "MM/DD/YYYY HH:MM PST" form
+    *   props.registrationCloseDate EX: "12/25/2023 10:00";
+    *       ^ MUST BE IN "MM/DD/YYYY HH:MM" form
     */
+    
+    // updates isRegistrationOpen according to if the registration date has passed.
+    // When props of <Event /> are changed, this does not change, so the page must be reloaded
     const closeDate = new Date(props.registrationCloseDate);
     let now  = new Date();
     const [isRegistrationOpen, setIsRegistrationOpen] = useState(now < closeDate);
     const UpdateTime=()=>{
         now =  new Date();
         setIsRegistrationOpen(now < closeDate);
-        console.log(isRegistrationOpen);
-        console.log(now);
-        console.log(closeDate);
     }
     setInterval(UpdateTime, 1000);
+
+
+    // checking to see if the window is mobile size
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 700);
+    const hideInfo = () => {
+        setIsMobile(window.innerWidth <= 700);
+    };
+
+    // this makes resizing window only happen once at the end of resizing
+    // it will look weird in the middle of resizing, but reduces a lot of lag
+    // "throttling"
+    var timeout = false;
+    window.addEventListener('resize', function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(hideInfo, 200);
+    });
+
+    // placeholder link "props.name"
     return (
-        <div className='event-container'>
-            <div>{props.name}</div>
-            <div>{props.time}, {props.date}</div>
-            <div>{props.capacity}</div>
-            <div>{props.location}</div>
-            <div>{isRegistrationOpen ? "OPEN" : "CLOSED"}</div>
-        </div>
+        <a href={props.name} className='event-link'>
+            <div className='event-container'>
+                <div className='event-item'>{props.name}</div>
+                <div className={isMobile? 'event-unnecessary':'event-item'}>{props.time}, {props.date}</div>
+                <div className={isMobile? 'event-unnecessary':'event-item'}>{props.capacity}</div>
+                <div className={isMobile? 'event-unnecessary':'event-item'}>{props.location}</div>
+                <div className='event-item'>{isRegistrationOpen ? "OPEN" : "CLOSED"}</div>
+            </div>
+        </a>
     )
 }
 
--- front/src/pages/Events.js
+++ front/src/pages/Events.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Events.css';
 import Event from '../Components/Event';
 
 function Events() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 700);
+    const hideInfo = () => {
+        setIsMobile(window.innerWidth <= 700);
+    };
+
+    window.addEventListener('resize', hideInfo);
     return (
         <div className="events-container">
             <div className="events-header">
-                <div className="event-header-item">Event</div>
-                <div className="event-header-item">Time/Date</div>
-                <div className="event-header-item">Capacity</div>
-                <div className="event-header-item">Location</div>
-                <div className="event-header-item">Register</div>
+                <div className="events-header-item" id="events-header-item-start">Event</div>
+                <div className={isMobile ?  "events-unnecessary": "events-header-item"}>Time/Date</div>
+                <div className={isMobile ?  "events-unnecessary": "events-header-item"}>Capacity</div>
+                <div className={isMobile ?  "events-unnecessary": "events-header-item"}>Location</div>
+                <div className="events-header-item">Register</div>
             </div>
-            <div className='event-horizontal-line'></div>
+            <div className='events-horizontal-line'></div>
+            <Event 
+                name="Junior Varsity Tryouts"
+                time="11:00 AM"
+                date="12/25/2024"
+                capacity="8/16"
+                location="Activity Annex"
+                registrationCloseDate="12/26/2024 16:48"
+            />
             <Event 
                 name="Varsity Tryouts"
                 time="11:00 AM"
                 date="12/25/2023"
                 capacity="5/16"
                 location="Activity Annex"
-                registrationCloseDate="12/25/2023 21:03 PST"
+                registrationCloseDate="12/25/2023 21:00"
             />
         </div>
     )
