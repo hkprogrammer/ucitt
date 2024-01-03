9a249280b24e1568fb905e8618f3f5caa61f85a4
Nathan Pham <phamnathan40@gmail.com>
2024-01-02 23:56:20 -0800

    Initial text and styling

---
+++ front/src/pages/SignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SignIn.css";
+
+function SignIn() {
+    return (
+        <div>
+            <h1 className="sign-in-welcome-text">Welcome to the UCITT Sign Up Page</h1>
+            <h2 className="sign-in-description-text">Sign up with an email if you'd like to participate in competitive events!</h2>
+        </div>
+    );
+}
+
+export default SignIn;
