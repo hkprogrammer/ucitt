9e221475ed953ae9fc4e14b29f15cd46a343685b
Hitoki <hkidahas@uci.edu>
2024-02-18 23:05:42 -0800

    added bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -14,6 +14,8 @@ import Ratings from './pages/Ratings';
 // eslint-disable-next-line
 import League from './pages/League';
 
+import Announcements from './pages/Announcements'
+
 // import { Link } from 'react-router-dom';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
@@ -33,8 +35,20 @@ function App() {
 			<Route path="/events" element = {<Events />} />
 			<Route path="/ratings" element = {<Ratings />} />
 			<Route path="/sign-in" element = {<SignIn />} />
+			<Route path="/announcements" element = {<Announcements />} />
 			{/*<Route path="/sign-up" element = {<SignUp />} />*/}
 		</Routes>
+		<script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
+
+		<script
+		src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+		crossorigin></script>
+
+		<script
+		src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+		crossorigin></script>
+
+		<script>var Alert = ReactBootstrap.Alert;</script>
 	</BrowserRouter>
   );
 }
---
+++ front/src/pages/Announcements.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+// import {useFetch} from '@uidotenv/usehooks';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion'
+import './Announcements.css';
+
+function Announcements(){
+    // props.events == array of event json
+    
+    
+
+    return (
+        <div>
+            <Button>hello</Button>
+            
+        </div>
+    )
+}
+
+export default Announcements
